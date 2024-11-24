3ee0b373d83a652a2711574c5c85fdb85561ffbd
abeni-al7 <abenezeralebachew3@gmail.com>
2024-11-24 17:27:03 +0300

    Implement PUT and DELETE apis for /books

--- book_collection/books/views.py
+++ book_collection/books/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Book
@@ -20,4 +20,34 @@ def book_list(request):
         return JsonResponse({'id': book.id}, status=201)
     else:
         return JsonResponse({'error': 'Invalid request'}, status=400)
+
+@csrf_exempt
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(pk=id)
+    except Book.DoesNotExist:
+        return JsonResponse({'error': 'Book not found'}, status=404)
+
+    if request.method == 'GET':
+        data = {
+            'id': book.id,
+            'title': book.title,
+            'author': book.author,
+            'isbn': book.isbn,
+            'published_year': book.published_year,
+        }
+        return JsonResponse(data)
+    elif request.method == 'PUT':
+        data = json.loads(request.body)
+        book.title = data.get('title', book.title)
+        book.author = data.get('author', book.author)
+        book.isbn = data.get('isbn', book.isbn)
+        book.published_year = data.get('published_year', book.published_year)
+        book.save()
+        return JsonResponse({'message': 'Book updated successfully'})
+    elif request.method == 'DELETE':
+        book.delete()
+        return JsonResponse({'message': 'Book deleted successfully'}, status=204)
+    else:
+        return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
 
