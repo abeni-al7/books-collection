da8728029c200be4c3f8748423dabdaeccae8294
abeni-al7 <abenezeralebachew3@gmail.com>
2024-11-24 17:03:25 +0300

    Implement POST books api

--- book_collection/books/views.py
+++ book_collection/books/views.py
@@ -1,8 +1,23 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import Book
 
+@csrf_exempt
 def book_list(request):
-    books = Book.objects.all()
-    data = list(books.values())
-    return JsonResponse(data, safe=False)
+    if request.method == 'GET':
+        books = Book.objects.all()
+        data = {"books": list(books.values())}
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        book = Book.objects.create(
+            title=data['title'],
+            author=data['author'],
+            isbn=data['isbn'],
+            published_year=data['published_year']
+        )
+        return JsonResponse({'id': book.id}, status=201)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
 
